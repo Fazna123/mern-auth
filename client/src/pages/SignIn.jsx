a2878bed31d1a51a7bf873b1b8fa66eb66917d3d
Fasna123 <fasnanishar@gmail.com>
2023-12-19 17:24:52 +0530

    Added Rdux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,24 @@
 import {Link, useNavigate} from 'react-router-dom'
 import { useState } from 'react';
+import { signInSuccess, signInStart, signInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error,setError]=useState(false);
-  const [Loading,setLoading]= useState(false)
+  const {loading, error}=useSelector((state) => state.user);
+  
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({...formData,[e.target.id]:e.target.value})
     
   }
+  
   const handleSubmit = async(e) => {
     e.preventDefault();
     try{
-      setLoading(true);
-      setError(false)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',{
         method:'POST',
         headers:{
@@ -24,15 +28,15 @@ function SignIn() {
         body:JSON.stringify(formData)
       });
       const data = await res.json();
-      setLoading(false);
+      
       if(data.success === false){
-        setError(true)
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate('/')
     }catch(error){
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error))
     }
     
   }
@@ -42,18 +46,18 @@ function SignIn() {
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}> 
         <input type='email' placeholder='Email' id='email' className="bg-slate-100 p-3 rounded-lg"  onChange={handleChange}/>
         <input type='password' placeholder='Password' id='password' className="bg-slate-100 p-3 rounded-lg" onChange={handleChange} />
-        <button disabled={Loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase 
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase 
         hover:opacity-95 disabled:opacity-80">
-          {Loading?'Loading...':'Sign Up'}
+          {loading?'Loading...':'Sign In'}
         </button>
       </form >
       <div className="flex gap-5 mt-5">
         <p>Dont have an account?</p>
         <Link to='/sign-up'>
-          <span className="text-blue-500">Sign In</span>
+          <span className="text-blue-500">Sign Up</span>
         </Link>       
       </div> 
-       <p className="text-red-700 mt-5">{error && 'Something went wrong'}</p>
+       <p className="text-red-700 mt-5">{error ? error || 'Something went wrong': ''}</p>
     </div>
   )
 }
